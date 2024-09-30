141ce6b2b2db68ab9b0fdedb54bb06e097bc5100
izz133 <67450066+izz133@users.noreply.github.com>
2024-09-30 08:22:13 +0800

    Update allowedOrigins.mjs

--- config/allowedOrigins.mjs
+++ config/allowedOrigins.mjs
@@ -1,5 +1,6 @@
 const allowedOrigins = [
-    'https://isc-vessel-tracker.onrender.com'
+    'https://isc-vessel-tracker.onrender.com',
+    'http://localhost:3000'
 ]
 
 //enter domain kalau mahu serve the app at other DNS
